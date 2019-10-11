160511f6c6be78ec8bb2725a27344d6d7daec356
Surendran <surendran@hasura.io>
2019-10-11 19:11:53 +0530

    loading fix

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -46,47 +46,6 @@ const ListItem = styled(({ className, active, level, ...props }) => {
   }
 `;
 
-const Sidebar = styled('aside')`
-  width: 100%;
-  /* background-color: rgb(245, 247, 249); */
-  /* border-right: 1px solid #ede7f3; */
-  height: 100vh;
-  overflow: auto;
-  position: fixed;
-  padding-left: 0px;
-  position: -webkit-sticky;
-  position: -moz-sticky;
-  position: sticky;
-  top: 0;
-  padding-right: 0;
-  background-color: #372476;
-  /* Safari 4-5, Chrome 1-9 */
-  background: linear-gradient(#372476, #3b173b);
-  background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#372476), to(#3b173b));
-  /* Safari 5.1, Chrome 10+ */
-  background: -webkit-linear-gradient(top, #372476, #3b173b);
-  /* Firefox 3.6+ */
-  background: -moz-linear-gradient(top, #372476, #3b173b);
-  /* IE 10 */
-  background: -ms-linear-gradient(top, #372476, #3b173b);
-  /* Opera 11.10+ */
-  background: -o-linear-gradient(top, #372476, #3b173b);
-  @media only screen and (max-width: 767px) {
-    padding-left: 0px;
-    background-color: #372476;
-    background: #372476;
-  }
-  @media (min-width: 767px) and (max-width:1023px)
-  {
-    padding-left: 0;
-  }
-  @media only screen and (max-width: 1023px) {
-    width: 100%;
-    /* position: relative; */
-    height: 100vh;
-  }
-`;
-
 
 const Divider = styled(props => (
   <li {...props}>
@@ -123,7 +82,7 @@ const SidebarLayout = ({location}) => (
     `}
     render={({allMdx}) => {
       return (
-        <Sidebar>
+        <aside className={'SidebarAside'}>
           <ul className={'sideBarUL'}>
             <Tree
               edges={allMdx.edges}
@@ -140,7 +99,7 @@ const SidebarLayout = ({location}) => (
               }
             })}
           </ul>
-        </Sidebar>
+        </aside>
       );
     }}
   />
